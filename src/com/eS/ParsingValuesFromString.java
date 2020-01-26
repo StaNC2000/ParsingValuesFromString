285b1d9b6073b9d0072cc5b6d24ca21bacf70adf
es <StaNC2000>
2020-01-26 11:23:14 +0100

    first parsing example added

--- src/com/eS/ParsingValuesFromString.java
+++ src/com/eS/ParsingValuesFromString.java
@@ -4,5 +4,10 @@ public class ParsingValuesFromString {
 
     public static void main(String[] args) {
 	// write your code here
+        String numberAsString = "2018";
+        System.out.println("numberAsString " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+
     }
 }
